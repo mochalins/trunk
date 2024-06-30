6826a19fa03a89955fc6946ac5efc2e83dfe7cfc
mochalins <117967760+mochalins@users.noreply.github.com>
2024-07-01 01:25:38 +0900

    feat: Tree object

--- src/Object/Tree.zig
+++ src/Object/Tree.zig
@@ -2,7 +2,7 @@
 const Tree = @This();
 
 const std = @import("std");
-const h = @import("../hash.zig");
+const Sha1 = @import("../hash.zig").Sha1;
 
 allocator: std.mem.Allocator,
 entries: []Entry,
@@ -11,9 +11,9 @@ pub const Entry = struct {
     /// POSIX file mode.
     mode: FileMode,
     /// Path of entry, with trailing separators stripped.
-    path: []const u8,
+    path: []u8,
     /// Hash of entry.
-    hash: h.Sha1,
+    hash: Sha1,
 
     pub const FileMode = struct {
         kind: Kind,
@@ -22,6 +22,22 @@ pub const Entry = struct {
             file = 0o10,
             directory = 0o04,
         };
+
+        pub fn write(self: @This(), writer: std.io.AnyWriter) !usize {
+            switch (self.kind) {
+                .file => try writer.writeAll("10"),
+                .directory => try writer.writeAll("04"),
+            }
+            var perms_buf: [4]u8 = undefined;
+            try writer.writeAll(std.fmt.bufPrintIntToSlice(
+                &perms_buf,
+                self.permissions,
+                8,
+                .lower,
+                .{ .fill = '0', .width = 4 },
+            ));
+            return 6;
+        }
 
         pub fn parse(str: []const u8) !@This() {
             if (str.len > 6 or str.len < 5) {
@@ -42,6 +58,23 @@ pub const Entry = struct {
             return result;
         }
     };
+
+    pub fn write(self: Entry, writer: std.io.AnyWriter) !usize {
+        var out_len: usize = 0;
+        out_len += try self.mode.write(writer);
+        try writer.writeByte(' ');
+        out_len += 1;
+        try writer.writeAll(self.path);
+        out_len += self.path.len;
+        try writer.writeByte(0);
+        out_len += 1;
+        try self.hash.formatHexWriter(writer);
+        out_len += Sha1.hex_length;
+        try writer.writeByte('\n');
+        out_len += 1;
+
+        return out_len;
+    }
 
     pub fn less(_: void, a: @This(), b: @This()) bool {
         const a_len: usize = a.path.len +
@@ -105,9 +138,12 @@ pub fn parse(allocator: std.mem.Allocator, str: []const u8) !Tree {
         const rem_line = mode_split.rest();
         var path_split = std.mem.splitScalar(u8, rem_line, 0);
 
-        result.entries[current_entry].path = path_split.first();
+        const path = path_split.first();
+        result.entries[current_entry].path = try allocator.alloc(u8, path.len);
+        @memcpy(result.entries[current_entry].path, path);
+
         result.entries[current_entry].hash =
-            try h.Sha1.parseHex(path_split.rest());
+            try Sha1.parseHex(path_split.rest());
 
         current_entry += 1;
     }
@@ -118,7 +154,30 @@ pub fn parse(allocator: std.mem.Allocator, str: []const u8) !Tree {
 /// Write to provided writer, returning number of bytes written if
 /// successful.
 pub fn write(self: Tree, writer: std.io.AnyWriter) !usize {
-    _ = self;
-    _ = writer;
-    return 0;
+    var out_len: usize = 0;
+    for (self.entries) |entry| {
+        out_len += try entry.write(writer);
+    }
+    return out_len;
+}
+
+test {
+    const test_lines =
+        "103746 my_object_path\x00ae90f12eea699729ed24555e40b9fd669da12a12\n" ++
+        "043436 my_other_object\x00e8bfe5af39579a7e4898bb23f3a76a72c368cee6\n";
+
+    const parsed_tree: Tree = try Tree.parse(
+        std.testing.allocator,
+        test_lines,
+    );
+    defer parsed_tree.deinit();
+
+    var write_tree_buf: [test_lines.len]u8 = undefined;
+    var stream = std.io.fixedBufferStream(&write_tree_buf);
+    const write_tree_len = try parsed_tree.write(stream.writer().any());
+    try std.testing.expectEqualSlices(
+        u8,
+        test_lines,
+        write_tree_buf[0..write_tree_len],
+    );
 }
